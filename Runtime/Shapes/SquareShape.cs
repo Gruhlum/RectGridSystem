8c8b119f1524de7aab5aab68afd63564654fc1ec
HexTecGames <Patrick.Hellwig93@gmail.com>
2025-06-21 12:30:55 +0200

    fixes

--- Runtime/Shapes/SquareShape.cs
+++ Runtime/Shapes/SquareShape.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using HexTecGames.Basics;
-using HexTecGames.GridBaseSystem;
 using HexTecGames.GridBaseSystem.Shapes;
 using UnityEngine;
 
